14acedc460ed659d9d7afeade7607ed462d155d2
krobz <nuaazh@outlook.com>
2021-09-07 23:06:52 +0800

    Add the post

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -15,4 +15,10 @@ public interface DiscussPostMapper {
     // 如果方法只有一个参数并且在<if>里使用，就必须加别名
     int selectDiscussPostRows(@Param("userId") int userId);
 
+    int insertDiscussPost(DiscussPost discussPost);
+
+    DiscussPost selectDiscussPostById(int id);
+
+    int updateCommentCount(int id, int commentCount);
+
 }
